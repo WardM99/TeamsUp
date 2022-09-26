bb592ee9ad4d8818fa5c948c7092fe5860595b48
Ward Meersman <wardmeersman@gmail.com>
2022-09-26 22:20:25 +0200

    added code, still need to be tested

--- backend/src/app/logic/cards.py
+++ backend/src/app/logic/cards.py
@@ -2,9 +2,25 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.models import Game, Card
-from src.database.crud.card import get_card_by_id, add_card_to_game
+from src.database.crud.card import get_card_by_id, add_card_to_game, get_random_card, update_card, reset_cards_game
 
 async def logic_add_card_to_game(database: AsyncSession, game: Game, card_id: Card) -> None:
     """Logic to add a card to a game"""
     card: Card = await get_card_by_id(database, card_id)
     await add_card_to_game(database, card, game)
+
+
+async def logic_get_random_card(database: AsyncSession, game: Game) -> Card:
+    """Logic to get a random card"""
+    return await get_random_card(database, game)
+
+
+async def logic_update_card(database: AsyncSession, game: Game, card_id: int) -> None:
+    """Logic to update a card"""
+    card: Card = await get_card_by_id(database, card_id)
+    await update_card(database, game, card)
+
+
+async def logic_reset_cards(database: AsyncSession, game: Game) -> None:
+    """Logic to reset all cards of a game"""
+    await reset_cards_game(database, game)
--- backend/src/app/routers/games/cards/cards.py
+++ backend/src/app/routers/games/cards/cards.py
@@ -6,7 +6,7 @@ from starlette import status
 from src.database.database import get_session
 from src.app.logic.players import require_player
 from src.app.logic.games import logic_get_game_by_id
-from src.app.logic.cards import logic_add_card_to_game
+from src.app.logic.cards import logic_add_card_to_game, logic_get_random_card, logic_update_card
 from src.database.models import Game, Player, Card
 from src.database.schemas.card import InputCard
 
@@ -18,7 +18,7 @@ async def get_next_card(
                         game: Game = Depends(logic_get_game_by_id)
                         ):
     """Get next card"""
-    return {"details": "Not Implemented"}
+    return await logic_get_random_card(database, game)
 
 
 @card_router.post("", dependencies=[Depends(require_player)], status_code=status.HTTP_204_NO_CONTENT)
@@ -30,12 +30,16 @@ async def add_card(input_card: InputCard,
 
 
 @card_router.post("/{card_id}", dependencies=[Depends(require_player)])
-async def correct_card(database: AsyncSession = Depends(get_session)):
+async def correct_card(card_id: int,
+                       database: AsyncSession = Depends(get_session),
+                       game: Game = Depends(logic_get_game_by_id)):
     """Card is guessed correctly"""
-    return {"details": "Not Implemented"}
+    await logic_update_card(database, game, card_id)
 
 
 @card_router.patch("/{card_id}")
-async def overrule_previous_card(database: AsyncSession = Depends(get_session), owner: Player = Depends(require_player)):
+async def overrule_previous_card(card_id: int,
+                       database: AsyncSession = Depends(get_session),
+                       game: Game = Depends(logic_get_game_by_id)):
     """Overrule the previous card when selected wrongly"""
-    return {"details": "Not Implemented"}
+    await logic_update_card(database, game, card_id)
