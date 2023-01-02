9133893299106e19d511919350adc7a4f5e01b89
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2023-01-02 19:55:48 +0100

    🎉Init frontend (#90)
    * start frontend
    
    * start login
    
    * delete unused tests

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PlayersLogin from './players/PlayersLogin';
+
+function App() {
+  return (
+   <PlayersLogin></PlayersLogin>
+  );
+}
+
+export default App;
