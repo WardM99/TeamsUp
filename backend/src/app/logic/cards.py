d5177c6931b79479c57c1acc7a86bdb080543de0
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2023-01-14 01:41:15 +0100

    👷added add cards route (#130)

--- backend/src/app/logic/cards.py
+++ backend/src/app/logic/cards.py
@@ -6,7 +6,8 @@ from src.database.crud.card import (get_card_by_id,
                                     add_card_to_game,
                                     get_random_card,
                                     update_card,
-                                    reset_cards_game)
+                                    reset_cards_game,
+                                    add_cards_to_database)
 
 async def logic_add_card_to_game(database: AsyncSession, game: Game, card_id: int) -> None:
     """Logic to add a card to a game"""
@@ -29,3 +30,8 @@ async def logic_update_card(database: AsyncSession, game: Game, card_id: int) ->
 async def logic_reset_cards(database: AsyncSession, game: Game) -> None:
     """Logic to reset all cards of a game"""
     await reset_cards_game(database, game)
+
+
+async def logic_add_cards_to_database(database: AsyncSession) -> None:
+    """Logic to add cards to database"""
+    await add_cards_to_database(database)
