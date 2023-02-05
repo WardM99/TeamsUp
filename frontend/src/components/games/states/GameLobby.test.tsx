9e26244cddf597b96f4c94312c89a701c1b368ac
Ward Meersman <wardmeersman@gmail.com>
2023-02-05 15:51:08 +0100

    prettier & linting

--- frontend/src/components/games/states/GameLobby.test.tsx
+++ frontend/src/components/games/states/GameLobby.test.tsx
@@ -1,30 +1,28 @@
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import GameLobby from "./GameLobby";
-import { Game } from "../../../data/interfaces/games";
 import { Team } from "../../../data/interfaces/teams";
 import { Player } from "../../../data/interfaces";
 import { getTeams, joinTeam } from "../../../utils/api/teams";
 import { nextStatus } from "../../../utils/api/games";
-import { useParams } from "react-router-dom";
 
 const team1: Team = {
-    teamId: 1,
-    gameId: 2,
-    teamName: "team 1",
-    players: [],
+  teamId: 1,
+  gameId: 2,
+  teamName: "team 1",
+  players: [],
 };
 
 const player: Player = {
-    playerId: 1,
-    name: "Jos",
-  }
-  
+  playerId: 1,
+  name: "Jos",
+};
+
 const team2: Team = {
-    teamId: 2,
-    gameId: 2,
-    teamName: "team 2",
-    players: [player],
+  teamId: 2,
+  gameId: 2,
+  teamName: "team 2",
+  players: [player],
 };
 /*
 const game: Game = {
@@ -40,66 +38,65 @@ const game: Game = {
 };
 */
 jest.mock("../../../utils/api/teams", () => ({
-getTeams: jest.fn(() => Promise.resolve({ teams: [team1, team2] })),
-joinTeam: jest.fn(),
+  getTeams: jest.fn(() => Promise.resolve({ teams: [team1, team2] })),
+  joinTeam: jest.fn(),
 }));
 
 jest.mock("../../../utils/api/cards", () => ({
-getCards: jest.fn(() => Promise.resolve({ cards: [] })),
-addCardToGame: jest.fn(),
+  getCards: jest.fn(() => Promise.resolve({ cards: [] })),
+  addCardToGame: jest.fn(),
 }));
 
 jest.mock("../../../utils/api/games", () => ({
-nextStatus: jest.fn(),
+  nextStatus: jest.fn(),
 }));
 
-
 const props = {
-    player
+  player,
 };
 
 describe("GameLobby", () => {
-    it("should render the teams", async ()=>{
-        render(<GameLobby {...props} />)
-        expect(getTeams).toBeCalledTimes(1);
-        await waitFor(() => {
-            const card1 = screen.getByTestId("TeamCardId1");
-            expect(card1).toBeInTheDocument();
-        })
-        await waitFor(() => {
-            const card2 = screen.getByTestId("TeamCardId2");
-            expect(card2).toBeInTheDocument();
-        })
+  it("should render the teams", async () => {
+    render(<GameLobby {...props} />);
+    expect(getTeams).toBeCalledTimes(1);
+    await waitFor(() => {
+      const card1 = screen.getByTestId("TeamCardId1");
+      expect(card1).toBeInTheDocument();
     });
-    it("should have a disabled button when the player is already in the team", async () => {
-        render(<GameLobby {...props} />)
-        await waitFor(() => {
-            const button = screen.getByText("Join team 2")
-            expect(button).not.toBeEnabled();
-        })
+    await waitFor(() => {
+      const card2 = screen.getByTestId("TeamCardId2");
+      expect(card2).toBeInTheDocument();
     });
-    it("should have an enabled button when the player is already in the team", async () => {
-        render(<GameLobby {...props} />)
-        let button:HTMLElement;
-        await waitFor(() => {
-            button = screen.getByText("Join team 1");
-            expect(button).toBeEnabled();
-        }).then(() => {
-            fireEvent.click(button);
-            expect(joinTeam).toBeCalledTimes(1);
-            //expect(joinTeam).toBeCalledWith(2,1)
-        });
+  });
+  it("should have a disabled button when the player is already in the team", async () => {
+    render(<GameLobby {...props} />);
+    await waitFor(() => {
+      const button = screen.getByText("Join team 2");
+      expect(button).not.toBeEnabled();
     });
-    it("should verify that the nextStatus function is called with the correct parameters when button is clicked", async () =>{
-        render(<GameLobby {...props} />)
-        let button:HTMLElement;
-        await waitFor(() => {
-            button = screen.getByText("Start suggesting cards")
-            expect(button).toBeInTheDocument();
-        }).then(() => {
-            fireEvent.click(button);
-            expect(nextStatus).toBeCalledTimes(1);
-            //expect(nextStatus).toBeCalledWith(2);
-        });
+  });
+  it("should have an enabled button when the player is already in the team", async () => {
+    render(<GameLobby {...props} />);
+    let button: HTMLElement;
+    await waitFor(() => {
+      button = screen.getByText("Join team 1");
+      expect(button).toBeEnabled();
+    }).then(() => {
+      fireEvent.click(button);
+      expect(joinTeam).toBeCalledTimes(1);
+      // expect(joinTeam).toBeCalledWith(2,1)
     });
-})
+  });
+  it("should verify that the nextStatus function is called with the correct parameters when button is clicked", async () => {
+    render(<GameLobby {...props} />);
+    let button: HTMLElement;
+    await waitFor(() => {
+      button = screen.getByText("Start suggesting cards");
+      expect(button).toBeInTheDocument();
+    }).then(() => {
+      fireEvent.click(button);
+      expect(nextStatus).toBeCalledTimes(1);
+      // expect(nextStatus).toBeCalledWith(2);
+    });
+  });
+});
--- frontend/src/utils/api/cards.test.ts
+++ frontend/src/utils/api/cards.test.ts
@@ -1,5 +1,4 @@
 import * as api from "./api";
-import { getTeams } from "./teams";
 import { Cards, Card } from "../../data/interfaces/cards";
 import { addCardToGame, getCards } from "./cards";
 
@@ -15,92 +14,94 @@ jest.mock("./api", () => {
 });
 
 describe("getCards", () => {
-    it("should return a list of cards on success", async () => {
-        const card1: Card = {
-            cardId: 1,
-            points: 10,
-            topic: "History"
-        };
-        const card2: Card = {
-            cardId: 2,
-            points: 5,
-            topic: "Geography"
-        };
-        const card3: Card = {
-            cardId: 3,
-            points: 15,
-            topic: "Science"
-        };
-        const card4: Card = {
-            cardId: 4,
-            points: 20,
-            topic: "Mathematics"
-        };
-        const cards: Cards = {
-            cards: [card1, card2, card3, card4]
-        };
-        const mockResponse = {
-            status: 200,
-            data: cards,
-        };
-        api.axiosInstance.get = jest.fn().mockReturnValue(mockResponse);
-        const response = await getCards();
-        expect(api.getHeaders).toBeCalledTimes(1);
-        expect(api.axiosInstance.get).toBeCalledTimes(1);
-        expect(api.axiosInstance.get).toBeCalledWith("/cards", {
-          headers: { Authorization: `bearer abc` },
-        });
-        expect(response).toEqual(mockResponse.data);
+  it("should return a list of cards on success", async () => {
+    const card1: Card = {
+      cardId: 1,
+      points: 10,
+      topic: "History",
+    };
+    const card2: Card = {
+      cardId: 2,
+      points: 5,
+      topic: "Geography",
+    };
+    const card3: Card = {
+      cardId: 3,
+      points: 15,
+      topic: "Science",
+    };
+    const card4: Card = {
+      cardId: 4,
+      points: 20,
+      topic: "Mathematics",
+    };
+    const cards: Cards = {
+      cards: [card1, card2, card3, card4],
+    };
+    const mockResponse = {
+      status: 200,
+      data: cards,
+    };
+    api.axiosInstance.get = jest.fn().mockReturnValue(mockResponse);
+    const response = await getCards();
+    expect(api.getHeaders).toBeCalledTimes(1);
+    expect(api.axiosInstance.get).toBeCalledTimes(1);
+    expect(api.axiosInstance.get).toBeCalledWith("/cards", {
+      headers: { Authorization: `bearer abc` },
     });
-    it("should return undefined when the API call fails", async () => {
-        api.axiosInstance.get = jest.fn().mockReturnValue({
-          status: 400,
-          data: { error: "invalid login credentials" },
-        });
-        const response = await getCards();
-        expect(response).toEqual(undefined);
+    expect(response).toEqual(mockResponse.data);
+  });
+  it("should return undefined when the API call fails", async () => {
+    api.axiosInstance.get = jest.fn().mockReturnValue({
+      status: 400,
+      data: { error: "invalid login credentials" },
     });
-    
-    it("should return undefined when an unexpected error occurs", async () => {
-        api.axiosInstance.get = jest.fn().mockImplementation(() => {
-          throw new Error("unexpected error");
-        });
-        const response = await getCards();
-        expect(response).toEqual(undefined);
+    const response = await getCards();
+    expect(response).toEqual(undefined);
+  });
+
+  it("should return undefined when an unexpected error occurs", async () => {
+    api.axiosInstance.get = jest.fn().mockImplementation(() => {
+      throw new Error("unexpected error");
     });
+    const response = await getCards();
+    expect(response).toEqual(undefined);
+  });
 });
 
 describe("addCardToGame", () => {
-    it("should return 204 when everything goes well", async () => {
-        api.axiosInstance.post = jest.fn().mockReturnValue({
-            status: 204,
-        });
-        const response = await addCardToGame(1,1);
-        expect(api.getHeaders).toBeCalledTimes(1);
-        expect(api.axiosInstance.post).toBeCalledTimes(1);
-        expect(api.axiosInstance.post).toBeCalledWith("/games/1/cards", 
-        {
-            card_id: 1
-        },
-        {
-          headers: { Authorization: `bearer abc` },
-        });
-        expect(response).toEqual(204);
+  it("should return 204 when everything goes well", async () => {
+    api.axiosInstance.post = jest.fn().mockReturnValue({
+      status: 204,
     });
-    
-    it("should return 400 when the API call fails", async () => {
-        api.axiosInstance.post = jest.fn().mockReturnValue({
-          status: 400,
-        });
-        const response = await addCardToGame(1,1);
-        expect(response).toEqual(400);
+    const response = await addCardToGame(1, 1);
+    expect(api.getHeaders).toBeCalledTimes(1);
+    expect(api.axiosInstance.post).toBeCalledTimes(1);
+    expect(api.axiosInstance.post).toBeCalledWith(
+      "/games/1/cards",
+      {
+        card_id: 1,
+      },
+      {
+        headers: { Authorization: `bearer abc` },
+      }
+    );
+    expect(response).toEqual(204);
+  });
+
+  it("should return 400 when the API call fails", async () => {
+    api.axiosInstance.post = jest.fn().mockReturnValue({
+      status: 400,
     });
-    
-    it("should return 500 when an unexpected error occurs", async () => {
-        api.axiosInstance.post = jest.fn().mockImplementation(() => {
-          throw new Error("unexpected error");
-        });
-        const response = await addCardToGame(1,1);
-        expect(response).toEqual(500);
+    const response = await addCardToGame(1, 1);
+    expect(response).toEqual(400);
+  });
+
+  it("should return 500 when an unexpected error occurs", async () => {
+    api.axiosInstance.post = jest.fn().mockImplementation(() => {
+      throw new Error("unexpected error");
     });
+    const response = await addCardToGame(1, 1);
+    expect(response).toEqual(500);
+  });
 });
