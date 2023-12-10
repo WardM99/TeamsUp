a0811fb0db9488463066d57b4e68e0ec91d26331
WardM99 <wardmeersman@gmail.com>
2023-12-10 10:18:31 +0100

    more tests

--- backend/tests/test_app/test_logic/test_players.py
+++ backend/tests/test_app/test_logic/test_players.py
@@ -2,6 +2,7 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.app.logic.players import logic_make_new_player, logic_get_player_by_name_and_password
+from src.app.exceptions.wrongcredentials import WrongCredentialsException
 from src.database.models import Player
 
 async def test_get_player_by_name_and_password(database_session: AsyncSession):
@@ -10,3 +11,19 @@ async def test_get_player_by_name_and_password(database_session: AsyncSession):
     assert player_new.name == "Joske"
     player: Player = await logic_get_player_by_name_and_password(database_session, "Joske", "PW1")
     assert player == player_new
+
+
+async def test_get_player_by_name_and_wrong_password(database_session: AsyncSession):
+    """Test the logic of get player by name and wrong password"""
+    player_new: Player = await logic_make_new_player(database_session, "Joske", "PW1")
+    assert player_new.name == "Joske"
+    with pytest.raises(WrongCredentialsException):
+        await logic_get_player_by_name_and_password(database_session, "Joske", "PW2")
+
+
+async def test_get_player_by_wrong_name_and_password(database_session: AsyncSession):
+    """Test the logic of get player by name and wrong password"""
+    player_new: Player = await logic_make_new_player(database_session, "Joske", "PW1")
+    assert player_new.name == "Joske"
+    with pytest.raises(WrongCredentialsException):
+        await logic_get_player_by_name_and_password(database_session, "joske", "PW1")
